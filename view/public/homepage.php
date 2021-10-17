b3177ab6909058bdd01dbc42c3a1e778826f6f31
QuentinFayt <quentin.fayt.p@gmail.com>
2021-10-17 03:45:08 +0200

    homepage css + footer

--- view/public/homepage.php
+++ view/public/homepage.php
@@ -1,5 +1,30 @@
 <?php
 ?>
 <main>
+    <img class="circle" src="./assets/img/circle_P.png" alt="circle" />
     <h1>Accueil</h1>
+    <div class="svg">
+        <?php
+        include "./assets/img/logo.php"
+        ?>
+    </div>
+
+    <article class="homepage">
+        <p>
+
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pharetra posuere neque sit amet
+            volutpat. Duis finibus interdum facilisis. Morbi vel sapien sed dolor hendrerit placerat id a dui. Integer
+            accumsan, arcu et rutrum efficitur, magna augue tempor quam, vel commodo diam nibh aliquam ex.
+            Curabitur odio leo, vulputate quis ligula vitae, venenatis aliquet lectus. In hac habitasse platea dictumst. Phasellus quis egestas lacus, sit amet dapibus diam. Donec nec ultrices metus.
+        </p>
+        <p>
+            In hac habitasse platea dictumst. Aliquam viverra mi ac eros vestibulum efficitur quis vitae justo. Nullam luctus, sapien id tempus eleifend, elit ex ornare quam, vitae pulvinar lectus nunc ac dolor. Duis ac diam quis mi rutrum gravida a vel elit. Suspendisse pharetra urna lacus, ac maximus arcu interdum a. Morbi vulputate faucibus nulla, at commodo ante porttitor at. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam et eros ac nibh eleifend rutrum. Suspendisse dapibus nisi ac nibh interdum, ac sollicitudin sem gravida. Duis a vehicula elit, a dictum libero. Fusce facilisis pretium mi, non tincidunt erat consectetur nec.
+
+            Nam eu massa quis enim porttitor egestas. Duis at sodales diam, eu porta velit. In ornare metus urna. Aliquam tristique laoreet lorem nec tristique. Quisque consectetur massa ut sem gravida vulputate. Praesent efficitur sem sed dui rutrum blandit ut at diam. Etiam et nisi vel odio consequat pharetra sed ac lorem. Vivamus posuere ex bibendum tellus euismod venenatis. Vivamus dignissim ipsum nibh. Etiam sed sem faucibus, vestibulum purus vitae, fermentum diam.
+
+            Sed imperdiet convallis blandit. Ut facilisis sed purus id vehicula. Sed non tempor dolor. Ut pharetra nulla in ligula lobortis, a scelerisque tellus porta. Nullam tincidunt imperdiet nisl, eget luctus nulla rutrum vitae. Duis porta convallis erat quis pretium. Nulla id gravida mauris.
+
+            Proin varius quam quam, vitae luctus lacus lobortis eu. Duis pharetra vehicula dolor at mattis. Nunc accumsan, tellus leo.
+        </p>
+    </article>
 </main>
