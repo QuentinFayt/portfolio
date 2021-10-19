7c409db30d5ad54af45c9000f2e3fa1336ce3c65
QuentinFayt <quentin.fayt.p@gmail.com>
2021-10-19 22:42:47 +0200

    mail protection/animation on writing and send

--- controller/mail.php
+++ controller/mail.php
@@ -4,25 +4,27 @@ if ($_POST) {
     $mail = strip_tags($_POST["user_mail"]);
     $message =  strip_tags($_POST["user_message"]);
 
+    if ($pseudo && $mail && $message) {
 
-    $toDev      = MAIL;
-    $subjectDev = "Message de " . $pseudo . " | Portfolio";
-    $messageDev = "Mail de l'utilisateur $pseudo : " . $mail . "\n" . $message;
-    $headersToDev =
-        'Content-Type: text/plain; charset="utf-8"' . "\r\n"  .
-        'From: ' . $mail  . "\r\n" .
-        'Reply-To: ' . $mail  . "\r\n" .
-        'X-Mailer: PHP/' . phpversion();
+        $toDev      = MAIL;
+        $subjectDev = "Message de " . $pseudo . " | Portfolio";
+        $messageDev = "Mail de l'utilisateur $pseudo : " . $mail . "\n" . $message;
+        $headersToDev =
+            'Content-Type: text/plain; charset="utf-8"' . "\r\n"  .
+            'From: ' . $mail  . "\r\n" .
+            'Reply-To: ' . $mail  . "\r\n" .
+            'X-Mailer: PHP/' . phpversion();
 
-    $toUsers = $mail;
-    $subjectUsers = "Quentin Fayt | Portfolio";
-    $messageUsers = "Merci $pseudo pour votre message!\nJe vous recontacte dans les plus brefs délais!\nBien à vous,\nQuentin Fayt";
-    $headersToUsers =
-        'Content-Type: text/plain; charset="utf-8"' . "\r\n"  .
-        'From: ' . MAIL . "\r\n" .
-        'Reply-To: ' . MAIL . "\r\n" .
-        'X-Mailer: PHP/' . phpversion();
+        $toUsers = $mail;
+        $subjectUsers = "Quentin Fayt | Portfolio";
+        $messageUsers = "Merci $pseudo pour votre message!\nJe vous recontacte dans les plus brefs délais!\nBien à vous,\nQuentin Fayt";
+        $headersToUsers =
+            'Content-Type: text/plain; charset="utf-8"' . "\r\n"  .
+            'From: ' . MAIL . "\r\n" .
+            'Reply-To: ' . MAIL . "\r\n" .
+            'X-Mailer: PHP/' . phpversion();
 
-    mail($toDev, $subjectDev, $messageDev, $headersToDev);
-    mail($toUsers, $subjectUsers, $messageUsers, $headersToUsers);
+        mail($toDev, $subjectDev, $messageDev, $headersToDev);
+        mail($toUsers, $subjectUsers, $messageUsers, $headersToUsers);
+    }
 }
--- public/assets/script.js
+++ public/assets/script.js
@@ -3,13 +3,14 @@ function toggle() {
 
   menu.className = menu.className === "menu" ? "menu in" : "menu";
 }
-function mail(arg, video) {
+/*function mail(arg, video) {
   arg === "play" ? video.play() : video.pause();
 }
 
 document.querySelector(".mail").addEventListener("mouseover", function () {
   mail("play", this);
-});
-document.querySelector(".mail").addEventListener("mouseleave", function () {
-  mail("pause", this);
-});
+});*/
+
+function mailEdit() {
+  document.querySelector(".mailE").play();
+}
