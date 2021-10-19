cd087dc3b18b83ab92538e0390b711206cc49c58
QuentinFayt <quentin.fayt.p@gmail.com>
2021-10-19 20:04:44 +0200

    target blank

--- view/public/footer.php
+++ view/public/footer.php
@@ -1,9 +1,9 @@
 <footer>
-    <a href="https://github.com/QuentinFayt">
+    <a href="https://github.com/QuentinFayt" target="_blank">
         <img class="foot_log mobile1" src="./assets/img/github.png" alt="github" />
         <p>Github</p>
     </a>
-    <a href="https://www.linkedin.com/in/quentin-fayt-6714b9174/">
+    <a href="https://www.linkedin.com/in/quentin-fayt-6714b9174/" target="_blank">
         <img class="foot_log mobile2" src="./assets/img/linkedin.png" alt="linkedin" />
         <p>Linkedin</p>
     </a>
