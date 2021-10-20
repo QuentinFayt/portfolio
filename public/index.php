e7457dfb2a0affee3e7924177a72497e817426d6
QuentinFayt <quentin.fayt.p@gmail.com>
2021-10-20 08:43:34 +0200

    test

--- public/index.php
+++ public/index.php
@@ -13,3 +13,4 @@ if (isset($_SESSION)) {
 include_once isset($_GET["p"]) && in_array($_GET["p"], WHITE_LIST) ?  "../view/public/" . $_GET['p'] . ".php" : "../view/public/homepage.php";
 
 require "../view/public/footer.php";
+echo "coucou";
