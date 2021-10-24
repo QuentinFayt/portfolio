45a185c3ce01b44d8b6b24be757e9ee42da86243
QuentinFayt <quentin.fayt.p@gmail.com>
2021-10-25 00:10:41 +0200

    cv

--- view/public/CV.php
+++ view/public/CV.php
@@ -1,5 +1,117 @@
 <?php
 ?>
 <main>
-    <h1>Curriculum Vitae</h1>
+    <h1>Curriculum Vitae | Quentin Fayt</h1>
+    <article class="CV">
+        <section>
+            <div class="titreCV">
+                <h2>Web-Développeur</h2>
+            </div>
+            <div class="contactCV">
+                <img class="circleCV" src="./assets/img/circle_P.png" alt="circle" />
+            </div>
+            <div class="experienceCV">
+                <h3>Expérience professionelle <?php include "./assets/img/learnO.svg" ?></h3>
+                <div class="blank"></div>
+                <div class="content">
+                    <p>Juin 2009 - Juillet 2012 • ULB • Jobiste</p>
+                    <ul>
+                        <li>
+                            Service financier, service achat, service courrier, immobiliers, archives.
+                        </li>
+                        <li>
+                            Encodages de factures, livraison de courrier, tri du courrier et des archives.
+                        </li>
+                    </ul>
+                    <p>Juillet 2014 - Août 2014 • Essenscia • Jobiste</p>
+                    <ul>
+                        <li>
+                            Electricité (câblage), informatique, réseaux
+                        </li>
+                        <li>
+                            Déplacement des boites de sols, installation des ordinateurs
+                        </li>
+                    </ul>
+                    <p>Novembre 2018 • Essenscia •Intérimaire</p>
+                    <ul>
+                        <li>
+                            Préparations des téléphones professionnels de tout le personnel.
+                        </li>
+                    </ul>
+                    <p>Février 2019 - Août 2019 • Monsieur Pneu • Indépendant via la Smart</p>
+                    <ul>
+                        <li>
+                            Réalisation d'une publicité low cost pour l'entreprise.
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="formationCV">
+                <h3>Formation <?php include "./assets/img/parcourtO.svg" ?></h3>
+                <div class="blank"></div>
+                <div class="content">
+                    <p>2021 - 2022 • Formation • CF2M </p>
+                    <ul>
+                        <li>
+                            Web-Développement
+                        </li>
+                    </ul>
+                    <p>2025 - 2018 • Formation • IFAPME </p>
+                    <ul>
+                        <li>
+                            Réalisation audio-visuel
+                        </li>
+                    </ul>
+                    <p>2014 - 2015 • Programmation • Ephec</p>
+                    <ul>
+                        <li>
+                            Option Informatique
+                        </li>
+                    </ul>
+                    <p>2011 - 2014 • Infographie • Haute école Albert Jacquard </p>
+                    <ul>
+                        <li>
+                            Option 3d temps réel - Programmation/Artistique
+                        </li>
+                    </ul>
+                    <p>2005 - 2011 • CESS • Collège Saint Etienne des Hayeffes </p>
+                    <ul>
+                        <li>
+                            Math 6h / Science 6h/ Anglais 4h / Néerlandais 4h
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="competenceCV">
+                <h3>Compétences <?php include "./assets/img/competencesO.svg" ?></h3>
+                <div class="blank"></div>
+                <div class="content">
+                    <p>Anglais - Français - Néerlandais</p>
+                    <pre>   C1        C2         A2</pre>
+                    <p>Informatique</p>
+                    <p class="text">
+                        Développement : Débutant en Javascript, PHP, SQL, HTML et CSS.</br>
+                        J'ai quelques notions en infographie : Photoshop, Zbrush, 3dsMax, Maya, 3dcoat, Illustrator, UDK.</br>
+                        Traitement du son: bon niveau en reaper et cubase, notion en Magix Music Maker.</br>
+                        Traitement de vidéo : Très bon niveau en Adobe Première, notion en Sony Vegas pro et After Effect.</br>
+                        Bureautique: bon niveau en Word et Excel.</br></br>
+
+                        Je n'ai pas peur d'apprendre en autodidacte et de me perfectionner à l'utilisation d'un programme, l'anglais étant un outil me permettant de comprendre assez facilement les notions d'un programme.
+                    </p>
+                </div>
+            </div>
+            <div class="interetCV">
+                <h3>Centres d'intérêt <?php include "./assets/img/interetO.svg" ?></h3>
+                <div class="blank"></div>
+                <div class="content">
+                    <p>Musique</p>
+                    <p class="text">Ecouter comme jouer, j'aime la musique, j'apprenais la guitare avec un professeur avant de devenir autodidacte à la basse et la guitare.</p>
+                    <p>Informatique</p>
+                    <p class="text">J'ai une bonne connaissance en informatique, je sais adapter les différentes composantes d'un pc à mon besoin ainsi que le monter moi-même.</p>
+                    <p>Jeux</p>
+                    <p class="text">Je suis passioné par les jeux de cartes à jouer du style Magic the Gathering. Je participe à des soirées autour de ce thème organisées régulièrement par "Case départ" à Louvain-La-Neuve. Ce style de jeu me permet d'épanouir ma réflexion et la mise en place de stratégie en coopération avec mes partenaires dans une compétition saine.</p>
+                </div>
+            </div>
+        </section>
+    </article>
 </main>
